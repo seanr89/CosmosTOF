999d3a23836d719c0b974f828b1296a5e7da852b
seanr89 <srafferty89@gmail.com>
2021-02-20 22:36:30 +0000

    Custom content

---
+++ ToFConsole/CustomContent.cs
@@ -0,0 +1,11 @@
+using System;
+using Newtonsoft.Json;
+
+namespace ToFConsole
+{
+    public class CustomContent
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+    }
+}
